c7a685d6aa9535d7aa8e6ed730a35287681895f9
Divyanshu241 <168987299+Divyanshu241@users.noreply.github.com>
2024-10-17 23:59:47 +0530

    Update cross round.py

--- cross round.py
+++ cross round.py
@@ -28,13 +28,12 @@ def checkwin(list):
     winh=0
     winv=0
     for i in range(3):
+        winh=sum(list[i])
         if winh==3 or winv==3 or wind1==3 or wind2==3: return 1 ; break
         elif winh==-3 or winv==-3 or wind1==-3 or wind2==-3: return -1 ; break
         elif turn==9: return 0
-        winh=0
         winv=0 
         for j in range (3):
-            winh+=list[i][j]
             winv+=list[j][i]
 
 #all the functions only work when list which contains the moves is named l.
@@ -58,28 +57,33 @@ def checkmoves():
     return probmoves
 
 def compmove():
-    moves=[]
-    losemoves=[]
+    moves=[0]
+    losemoves=[0]
     probmove=checkmoves()
     for i in probmove[0]:
         if probmove[0][max(probmove[0],key=probmove[0].get)]==probmove[0][i]: losemoves.append(i)
     for i in probmove[1]:
         if probmove[1][min(probmove[1],key=probmove[1].get)]==probmove[1][i]: moves.append(i)
     for i in losemoves:
+        losemoves.pop(0)
+        print(losemoves)
         testl=[row[:] for row in l]
         testl[(i-1)//3][(i-1)%3]=1
+        print(checkwin(testl))
         if checkwin(testl)==1: lose=i ; break
-        else: lose=0 
+        else: lose=0
     for i in moves:
+        moves.pop(0)
         testl=[row[:] for row in l]
         testl[(i-1)//3][(i-1)%3]=-1
         if checkwin(testl)==-1: win=i ; break
         else: win=0
+    print([win,lose,choice(moves)])
     return [win,lose,choice(moves)] 
 
 #compmove() returns a list [ winning move , losing move , random move ] if winning or losing move is 0 then there isn't amove like that. 
 
-l=[[-1,0,-1],[0,0,1],[1,0,0]]
+l=[[1,0,0],[0,-1,0],[0,-1,1]]
 turn=0
 
 display()
